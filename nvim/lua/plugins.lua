08c45bfe6e22ba626af267863cd6bfa90e8c2d35
kacparas <vilius.kasparas@gmail.com>
2026-05-14 14:49:59 +0200

    Added autoformatting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,217 +1,224 @@
 require("lazy").setup({
 
-    {
-      "shaunsingh/nord.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = false
-        vim.g.nord_disable_background = false
-        vim.g.nord_italic = false
-        require("nord").set()
-      end,
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = false
+      require("nord").set()
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
+    config = function()
+      require("telescope").setup()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      },
-      config = function()
-        require("telescope").setup()
-        require("telescope").load_extension("fzf")
-      end,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
     },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        window = { width = 35 },
+      })
+    end,
+  },
 
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-      },
-      config = function()
-        require("neo-tree").setup({
-          close_if_last_window = true,
-          window = { width = 35 },
-        })
-      end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      vim.cmd("TSUpdate")
+      require("nvim-treesitter.config").setup({
+        ensure_installed = {
+          "lua", "python", "r",
+          "svelte", "javascript", "typescript",
+          "html", "css", "markdown",
+        },
+        highlight = { enable = true },
+        indent = { enable = true, disable = { "python" } },
+      })
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        vim.cmd("TSUpdate")
-        require("nvim-treesitter.config").setup({
-          ensure_installed = {
-            "lua", "python", "r",
-            "svelte", "javascript", "typescript",
-            "html", "css", "markdown",
-          },
-          highlight = { enable = true },
-          indent = { enable = true, disable = { "python" } },
-        })
-      end,
-    },
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-      },
-      config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-            ["<C-k>"] = cmp.mapping.select_prev_item(),
-            ["<C-j>"] = cmp.mapping.select_next_item(),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          }),
-          sources = cmp.config.sources({
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "buffer" },
-            { name = "path" },
-          }),
-        })
-      end,
-    },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pyright", "ruff", "lua_ls", "svelte", "html", "cssls", "taplo",
+        },
+      })
+    end,
+  },
 
-    {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end,
-    },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    {
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = { "williamboman/mason.nvim", "hrsh7th/cmp-nvim-lsp" },
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "pyright", "ruff", "lua_ls", "svelte", "html", "cssls",
-          },
-        })
-      end,
-    },
+      vim.lsp.config("*", { capabilities = capabilities })
 
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = { "hrsh7th/cmp-nvim-lsp", "williamboman/mason-lspconfig.nvim" },
-      config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        vim.lsp.config("*", { capabilities = capabilities })
-
-        vim.lsp.config("lua_ls", {
-          settings = {
-            Lua = {
-              runtime = { version = "LuaJIT" },
-              workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-              diagnostics = { globals = { "vim" } },
-            },
-          },
-        })
-
-        vim.lsp.enable({ "pyright", "ruff", "lua_ls", "svelte", "html", "cssls" })
-      end,
-    },
-
-    {
-      "Vigemus/iron.nvim",
-      config = function()
-        require("iron.core").setup({
-          config = {
-            scratch_repl = true,
-            repl_definition = {
-              python = {
-                command = { "ipython", "--no-autoindent" },
-                format = require("iron.fts.common").bracketed_paste,
-              },
-            },
-            repl_open_cmd = require("iron.view").split.horizontal.botright(15),
-          },
-          keymaps = {
-            send_motion = "<space>rc",
-            visual_send = "<space>rc",
-            send_line = "<space>rl",
-            cr = "<space>r<cr>",
-            interrupt = "<space>r<space>",
-            exit = "<space>rq",
-            clear = "<space>rx",
-          },
-        })
-      end,
-    },
-
-    {
-      "R-nvim/R.nvim",
-      ft = { "r", "rmd" },
-      config = function()
-        require("r").setup({
-          R_app = "radian",
-          R_args = {},
-          rconsole_height = 15,
-        })
-      end,
-    },
-
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        require("lualine").setup({
-          options = {
-            theme = "nord",
-            globalstatus = true,
-          },
-          sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = { "venv-selector", "filetype" },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
-    },
-        })
-      end,
-    },
-
-    {
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup()
-      end,
-    },
-
-    {
-      "linux-cultist/venv-selector.nvim",
-      dependencies = { "nvim-telescope/telescope.nvim" },
-      opts = {
+      vim.lsp.config("lua_ls", {
         settings = {
-          options = {
-            notify_user_on_venv_activation = true,
-          }
-        }
-      },
-    },   
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
 
- })
+      vim.lsp.enable({ "pyright", "ruff", "lua_ls", "svelte", "html", "cssls", "taplo" })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.py", "*.toml", "*.lua" },
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end,
+  },
+
+  {
+    "Vigemus/iron.nvim",
+    config = function()
+      require("iron.core").setup({
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            python = {
+              command = { "ipython", "--no-autoindent" },
+              format = require("iron.fts.common").bracketed_paste,
+            },
+          },
+          repl_open_cmd = require("iron.view").split.horizontal.botright(15),
+        },
+        keymaps = {
+          send_motion = "<space>rc",
+          visual_send = "<space>rc",
+          send_line = "<space>rl",
+          cr = "<space>r<cr>",
+          interrupt = "<space>r<space>",
+          exit = "<space>rq",
+          clear = "<space>rx",
+        },
+      })
+    end,
+  },
+
+  {
+    "R-nvim/R.nvim",
+    ft = { "r", "rmd" },
+    config = function()
+      require("r").setup({
+        R_app = "radian",
+        R_args = {},
+        rconsole_height = 15,
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "nord",
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "venv-selector", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        }
+      }
+    },
+  },
+
+})
